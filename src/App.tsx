b97b9ac364cebca31a7a1e500120f835034496ce
[dyad] <sauravpa581@gmail.com>
2025-07-27 01:44:25 +0530

    [dyad] Streamlined the notification system - wrote 1 file(s), deleted 4 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -46,7 +45,6 @@ const App = () => (
       themes={['light', 'dark', 'warm', 'system']}
     >
       <TooltipProvider>
-        <Toaster />
         <Sonner />
         <BrowserRouter>
           <Routes>
