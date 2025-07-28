07ca3bb4e86166479e17fc2166e84f84259df201
[dyad] <sauravpa581@gmail.com>
2025-07-28 20:45:43 +0530

    [dyad] Executed a final, direct database fix to resolve all admin issues - wrote 1 file(s), deleted 2 file(s), executed 2 SQL queries

--- src/App.tsx
+++ src/App.tsx
@@ -33,10 +33,6 @@ import CommissionRules from "./pages/admin/CommissionRules";
 import Reporting from "./pages/admin/Reporting";
 import AdminLogin from "./pages/admin/AdminLogin";
 
-// Temporary Setup Page
-import FinalAdminCreation from "./pages/temp/FinalAdminCreation";
-
-
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -45,9 +41,6 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          {/* ONE-TIME SETUP ROUTE */}
-          <Route path="/final-admin-creation" element={<FinalAdminCreation />} />
-
           {/* Client Portal */}
           <Route element={<ProtectedRoute />}>
             <Route element={<PageLayout />}>
