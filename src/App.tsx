6812543d77f0dcbe92e15fc6adea30abbcf9cafa
[dyad] <sauravpa581@gmail.com>
2025-07-28 15:01:18 +0530

    [dyad] Created a final master reset to fix the admin account - wrote 2 file(s), executed 1 SQL queries

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,10 @@ import CommissionRules from "./pages/admin/CommissionRules";
 import Reporting from "./pages/admin/Reporting";
 import AdminLogin from "./pages/admin/AdminLogin";
 
+// Temporary Setup Page
+import MasterReset from "./pages/temp/MasterReset";
+
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -41,6 +45,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          {/* ONE-TIME SETUP ROUTE */}
+          <Route path="/master-reset" element={<MasterReset />} />
+
           {/* Client Portal */}
           <Route element={<ProtectedRoute />}>
             <Route element={<PageLayout />}>
