a26d6bdebad5f50d82ef7b622ccbce4ada8f7a24
[dyad] <sauravpa581@gmail.com>
2025-07-28 22:41:00 +0530

    [dyad] Restored the warm theme option - wrote 2 file(s)

--- src/components/layout/ModeToggle.tsx
+++ src/components/layout/ModeToggle.tsx
@@ -31,6 +31,9 @@ export function ModeToggle() {
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("warm")}>
+          Warm
+        </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
           System
         </DropdownMenuItem>
