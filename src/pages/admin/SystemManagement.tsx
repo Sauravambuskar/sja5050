e6b2d21571ace81e5d8cd5ee8dcfdde12f160a9b
[dyad] <sauravpa581@gmail.com>
2025-08-16 12:35:49 +0530

    [dyad] Implementing a dynamic ID Card Customizer for administrators. - wrote 5 file(s), executed 1 SQL queries

--- src/pages/admin/SystemManagement.tsx
+++ src/pages/admin/SystemManagement.tsx
@@ -25,7 +25,6 @@ import { BroadcastHistory } from "@/components/admin/BroadcastHistory";
 import { IdCardCustomizer } from "@/components/admin/IdCardCustomizer";
 import { MaintenanceMode } from "@/components/admin/MaintenanceMode";
 import { CompanyBankDetails } from "@/components/admin/CompanyBankDetails";
-import { AuthLayoutCustomizer } from "@/components/admin/AuthLayoutCustomizer";
 
 const triggerMaturityProcessing = async () => {
   const { data, error } = await supabase.functions.invoke('admin-trigger-maturities');
@@ -99,7 +98,6 @@ const SystemManagement = () => {
           <MaintenanceMode />
           <IdCardCustomizer />
           <CompanyBankDetails />
-          <AuthLayoutCustomizer />
         </div>
         <div className="space-y-6">
           <Card>
