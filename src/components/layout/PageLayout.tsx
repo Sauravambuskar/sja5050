8a6828d17934eab234c473690994f4081c01f5e6
[dyad] <sauravpa581@gmail.com>
2025-09-28 18:29:03 +0530

    [dyad] Fixed mobile layout instability by removing extra padding - wrote 1 file(s)

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -50,7 +50,7 @@ export const PageLayout = () => {
         </div>
         <div className="flex flex-col">
           <Header />
-          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 pb-20 md:pb-6">
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
             <Outlet context={{ handleViewUser }} />
           </main>
         </div>
